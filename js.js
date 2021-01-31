9a4a902b469f9b50e0be56b1db5d1b1e93dc223c
yahya-iyaseh <57312677+yahya-iyaseh@users.noreply.github.com>
2021-01-31 18:15:26 +0200

    V2

--- js.js
+++ js.js
@@ -1,35 +1,44 @@
-const correctAnswers = ['B', 'A', 'B', 'A' ];
+const correctAnswers = ['A', 'B', 'B', 'A', 'A'];
 const form = document.querySelector('.quiz-form');
 const result = document.querySelector('.result');
+const show = document.querySelector('#show');
+const boxShow = document.querySelector('#box-show');
+
+
+ show.addEventListener('click', e =>{
+  boxShow.classList.toggle('d-none');
+  show.classList.add('d-none');
+ });
 
 form.addEventListener('submit', e =>{
- e.preventDefault();
- let score = 0;
- const userAnswers = [form.q1, form.q2, form.q3, form.q4];
- userAnswers.forEach((answer, index) =>{
-  if(answer.value === correctAnswers[index]){
-   score +=( 1 / correctAnswers.length) * 100;
-   }
- }); 
+  e.preventDefault();
+  let score = 0;
+  const userAnswers = [form.q1, form.q2, form.q3, form.q4 ,form.q5];
+ // console.log(userAnswers);
+  userAnswers.forEach((answer, index) =>{
+   if(answer.value === correctAnswers[index]){
+      console.log(answer.value);
+      score +=( 1 / correctAnswers.length) * 100;
+    }
+  }); 
 
- 
+ // console.log(score);
   scrollTo(0,0);
+
  result.classList.remove('d-none');
 
-
 let out = 0;
+
 const timer = setInterval(() =>{
- result.querySelector('span').textContent =`${out}%`;
- if(out === score){
-  clearInterval(timer);
- }else{
+  result.querySelector('span').textContent =`${out}%`;
+  if(out === score){
+    clearInterval(timer);
+  }else{
   out++;
- }
-
-}, 10);
-
- 
-
+  }
+ }, 10);
+ boxShow.classList.toggle('d-none');
+ show.classList.remove('d-none');
 });
 
 
